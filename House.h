d247e04f10172bee5a3ad540463eab42837ebaf6
Ziyao Zhou <ziyaozhou@Ziyaos-MacBook-Pro.local>
2015-12-10 18:19:15 -0800

    commit

---
+++ House.h
@@ -0,0 +1,14 @@
+//
+//  House1.hpp
+//  CSE167 Spring 2015 Starter Code
+//
+//  Created by Ziyao Zhou on 10/8/15.
+//  Copyright © 2015 RexWest. All rights reserved.
+//
+
+#ifndef House1_hpp
+#define House1_hpp
+
+#include <stdio.h>
+
+#endif /* House1_hpp */
---
+++ Rasterizer.cpp
@@ -0,0 +1,9 @@
+//
+//  Rasterizer.cpp
+//  CSE167 Spring 2015 Starter Code
+//
+//  Created by Ziyao Zhou on 10/13/15.
+//  Copyright © 2015 RexWest. All rights reserved.
+//
+
+#include <stdio.h>
